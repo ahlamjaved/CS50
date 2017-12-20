f78206bbdaff607141fc8091c3fe639a392a1067
ahlamjaved <ahlam.ayesh91@gmail.com>
2017-12-20 17:17:15 -0600

    adding new programs from cs50 week 10 lecture on javascript

---
+++ Week10lecture/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $('#quote').submit(function() {
+
+        var url = '/quote?symbol=' + $('#symbol').val();
+        $.getJSON(url, function(data) {
+            alert(data.price);
+        });
+        return false;
+    });
+});
